e831258cf20c6c0e1fceda73f712e892753e5799
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-24 09:58:40 +0300

    Implement methods from the interfaces

--- src/main/java/dao/Sql2oEmployeeDao.java
+++ src/main/java/dao/Sql2oEmployeeDao.java
@@ -1,4 +1,67 @@
 package dao;
 
-public class Sql2oEmployeeDao {
+import models.Employee;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oEmployeeDao implements EmployeeDao {
+    private final Sql2o sql2o;
+    public Sql2oEmployeeDao(Sql2o sql2o){
+        this.sql2o=sql2o;
+    }
+
+    @Override
+    public void add(Employee employee) {
+        String sql = "INSERT INTO employees (name, position,role,department_id) VALUES (:title, :content, :writtenBy, :department_id)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(employee)
+                    .executeUpdate()
+                    .getKey();
+            employee.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public List<Employee> getAll() {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM employees")
+                    .executeAndFetch(Employee.class);
+        }
+    }
+
+    @Override
+    public Employee findById(int id) {
+        try (Connection con = sql2o.open()) {
+            return (Employee) con.createQuery("SELECT * FROM employees WHERE id=:id")
+                    .executeAndFetch(Employee.class);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from employees WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void clearAll() {
+        String sql = "DELETE from employees";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql).executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
 }
--- src/test/java/dao/Sql2oNewsDaoTest.java
+++ src/test/java/dao/Sql2oNewsDaoTest.java
@@ -3,10 +3,18 @@ package dao;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import models.News;
+import models.Department;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class Sql2oNewsDaoTest {
+    private Connection conn;
+    private Sql2oNewsDao newsDao;
+    private Sql2oDepartmentDao departmentDao;
+
 
     @Before
     public void setUp() throws Exception {
