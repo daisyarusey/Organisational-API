ee63a502a05aa77db829b1617095e2f4e4ae190c
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-24 09:52:50 +0300

    Add method to list news about the department

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -2,18 +2,19 @@ package dao;
 
 import models.Department;
 import models.Employee;
+import models.News;
 
 import java.util.List;
 
 public interface DepartmentDao {
-    //create restaurant
+    //create    department
     void add (Department department);
 
     //read
     List<Department> getAll();
     Department findById(int id);
     List<Employee> getAllEmployeesForADepartment(int department_id);
-
+    List<News> getAllNewsForDepartment(int department_id);
     //update
     void update(int id,String name, String description, int total_employees);
 
---
+++ src/test/java/dao/Sql2oNewsDaoTest.java
@@ -0,0 +1,38 @@
+package dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Sql2oNewsDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void getAllNewsByDepartment() {
+    }
+
+    @Test
+    public void deleteById() {
+    }
+
+    @Test
+    public void clearAll() {
+    }
+}
